d84f62afefefc09f57cabf0842d21072910c5fdc
Taufiqurrahman <rtaufiq426@gmail.com>
2025-01-14 19:03:37 +0700

    menambahkan contlorer

--- app/Http/Controllers/siswacontroller.php
+++ app/Http/Controllers/siswacontroller.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use view;
-use App\Model\Siswa;
+use App\Models\Siswa;
 use Illuminate\Http\Request;
 
-class siswacontroller extends Controller
+class SiswaController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -15,16 +14,17 @@ class siswacontroller extends Controller
      */
     public function index()
     {
-        return view('index'); 
-        'siswas' => Siswa::all()  
-    };
+        return view('index', [
+            'siswa' => Siswa::all()
+        ]);
+    }
 
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create( )
+    public function create()
     {
         return view('crud.create');
     }
@@ -32,68 +32,86 @@ class siswacontroller extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
         $rules = $request->validate([
-            'nama' => 'required|main:3',
+            'nama' => 'required|min:3',
             'kelas' => 'required',
-            'mis'=> 'required',
+            'nis' => 'required',
             'jk' => 'required',
             'alamat' => 'required',
             'hobi' => 'nullable'
-
-
         ]);
 
-        siswa::created($rules);
-        return redirect('/crud.crate');
+        Siswa::create($rules);
+
+        return redirect('/crud.create')->with('success', 'Data siswa berhasil ditambahkan');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
+        return view('crud.detail', [
+            'siswa' => Siswa::findOrFail($id)
+        ]);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
-        //
+        return view('crud.edit', [
+            'siswa' => Siswa::findOrFail($id)
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
+        $rules = $request->validate([
+            'nama' => 'required|min:3',
+            'kelas' => 'required',
+            'nis' => 'required',
+            'jk' => 'required',
+            'alamat' => 'required',
+            'hobi' => 'nullable'
+        ]);
+
+        $siswa = Siswa::findOrFail($id);
+        $siswa->update($rules);
+
+        return redirect('/crud')->with('success', 'Data siswa berhasil diperbarui');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
+        $siswa = Siswa::findOrFail($id);
+        $siswa->delete();
+
+        return redirect('/crud')->with('success', 'Data siswa berhasil dihapus');
     }
 }
